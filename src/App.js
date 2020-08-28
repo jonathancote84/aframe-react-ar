75586bfa70a9fecb3241a4f19b8f59141b925235
jonathancote84 <jonathancote92@gmail.com>
2020-08-28 14:01:59 -0500

    no tag

--- src/App.js
+++ src/App.js
@@ -11,21 +11,14 @@ function App() {
     arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
   >
    
-    <a-nft
-      type="nft"
-      url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
-      smooth="true"
-      smoothCount="10"
-      smoothTolerance=".01"
-      smoothThreshold="5"
-    >
+
       <a-entity
         gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
         scale="5 5 5"
         position="50 150 0"
       >
       </a-entity>
-    </a-nft>
+
     <a-entity camera></a-entity>
   </a-scene>
     </div>
